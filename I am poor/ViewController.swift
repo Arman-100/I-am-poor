e33900e816dbf2d999aff7817838b2560c9a3d9a
Arman Shoraka <armanshoraka@gmail.com>
2019-11-29 23:50:58 -0500

    Initial Commit

---
+++ I am poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am poor
+//
+//  Created by Arman Shoraka on 11/29/19.
+//  Copyright © 2019 Arman Shoraka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
